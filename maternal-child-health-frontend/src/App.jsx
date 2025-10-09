0e4776f40ae92fe913d87738f65fd5c263a28e81
Senzo Dubazana <senzoasanda6@gmail.com>
2025-10-09 13:30:26 +0200

    removed the conflicting import

--- maternal-child-health-frontend/src/App.jsx
+++ maternal-child-health-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./contexts/AuthContext";
 import AppRoutes from "./routes";
 
 function App() {
