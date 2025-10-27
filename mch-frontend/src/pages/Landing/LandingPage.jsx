6c3f1b4c0e55abe2b93e7a112299dfa486497993
Thabang Mposula <thabang.mposula@outlook.com>
2025-10-27 15:24:51 +0200

    Feat: Add Labour & Delivery and Immunisations pages; update routes for new services

--- mch-frontend/src/pages/Landing/LandingPage.jsx
+++ mch-frontend/src/pages/Landing/LandingPage.jsx
@@ -457,6 +457,8 @@ const LandingPage = () => {
                                 />
                             ))}
                         </div>
+
+                        <hr />
                     </div>
                 </section>
 
